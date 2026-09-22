27d2a879c09762d2d2aec93993fe244d70c1c172
Hanli Zhang <hanlizhang@Hanlis-MacBook-Pro.local>
2026-09-22 21:16:35 +0800

    Reset scroll position on route changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { AuthProvider } from "./auth/AuthContext.tsx";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop.tsx";
 import "./index.scss";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <AuthProvider>
         <App />
       </AuthProvider>
